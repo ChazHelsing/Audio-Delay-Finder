ed6136d2a8d56b21ee174fe26cf5dea9b4a2700d
Chaz <chazhelsing@icloud.com>
2016-03-10 10:12:11 -0500

    Basic App Functionality
    This is the basic app functionality

--- Audio Delay Finder/ViewController.h
+++ Audio Delay Finder/ViewController.h
@@ -10,6 +10,26 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *tempatureText;
+
+@property (weak, nonatomic) IBOutlet UITextField *distance;
+
+@property (weak, nonatomic) IBOutlet UILabel *AudioDelayFinderLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *enterTemapatureTextLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *distanceFromSourceTextLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *feetToMetersConvertionTextLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *feetToMetersConvertionOutputLabel;
+
+
+- (IBAction)CalculateButton:(id)sender;
+- (IBAction)backgroundTap:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *DelayTimeLabel;
+
 
 @end
 
--- Audio Delay Finder/ViewController.m
+++ Audio Delay Finder/ViewController.m
@@ -1,12 +1,11 @@
-//
 //  ViewController.m
-//  Audio Delay Finder
-//
+//  Audio Delay Finder for iPhone
+
 //  Created by chaz on 3/1/16.
 //  Copyright © 2016 Helsing Productions. All rights reserved.
-//
 
 #import "ViewController.h"
+
 
 @interface ViewController ()
 
@@ -17,11 +16,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+   // self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"back1.png"]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+    // Calculate Button
+- (IBAction)CalculateButton:(id)sender {
+    
+    // The input for the tempature number
+    float tempatureNumber = [[_tempatureText text] floatValue];
+    
+    // The input fo the distance number in feet
+    float distanceNumberFeet = [[_distance text] floatValue];
+    
+    // The conversion from feet to meters for the equation
+    float distanceNumberMeters = distanceNumberFeet / 3.28;
+    
+    
+    //  The calculation for the speed of sound
+    float speedOfSound = 331 + (0.597 * tempatureNumber);
+    
+    //  The calculation for the audio delay time
+    float audioDelayTime = distanceNumberMeters / speedOfSound;
+    
+    //  The audio delay time output of the calcualtions
+    NSString *output = [NSString stringWithFormat:@"%f", audioDelayTime ];
+    _DelayTimeLabel.text = output;
+    
+    //  The feet to meter convertion calcualtion
+    NSString *output2 = [NSString stringWithFormat:@"%f", distanceNumberMeters ];
+    _feetToMetersConvertionOutputLabel.text = output2;
+    
+    
+}
+    //  Making the keyboard dissapear on the screen with a background tap
+- (IBAction)backgroundTap:(id)sender {
+    [self.view endEditing:YES];
 }
 
 @end
